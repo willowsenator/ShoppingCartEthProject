442cf4b259299aa4430229bcc659a669625c6e57
willowsenator <willowsenator@gmail.com>
2023-10-14 01:14:09 +0200

    Show products and product details in front

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {Cart} from '../components/Cart.jsx';
 import {Products} from '../components/Products.jsx';
 import {Home} from '../components/Home.jsx';
 import {Product} from '../components/Product.jsx';
 
+const client = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}>
-          <Route index element={<Products/>}/>
-          <Route path='*' element={<Products/>}/>
-          <Route path='products' element={<Products/>}/>
-          <Route path='product/:id' element={<Product/>}/>
-          <Route path='cart' element={<Cart/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+            <Route index element={<Products/>}/>
+            <Route path='*' element={<Products/>}/>
+            <Route path='products' element={<Products/>}/>
+            <Route path='product/:id' element={<Product/>}/>
+            <Route path='cart' element={<Cart/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
