c1d87f857c86de4fe608e981a62f103571f1fd09
willowsenator <willowsenator@gmail.com>
2023-10-14 01:14:09 +0200

    Show products and product details in front

--- front/components/Product.jsx
+++ front/components/Product.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import {useQuery} from "react-query";
 
 export function Product(){
     let params = useParams();
-    return (<h1>Product {params.id}</h1>);
+    const {data, isLoading} = useQuery(`product_${params.id}`, async()=>{
+        const res = await fetch(`http://127.0.0.1:9800/product/${params.id}`, { method: "GET" });
+        return await res.json();
+    });
+
+    if(isLoading){
+        return (<div><h1>Loading...</h1></div>);
+    } else{
+        return (<div>
+            <h1>PRODUCT DETAILS</h1>
+            <table className="table"> 
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>QUANTITY_PER_UNIT</th>
+                        <th>UNIT_PRICE</th>
+                        <th>UNITS_IN_STOCK</th>
+                        <th>DISCONTINUED</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map(product=> (
+                            <tr>
+                                <td>{product.product_id}</td>
+                                <td>{product.product_name}</td>
+                                <td>{product.quantity_per_unit}</td>
+                                <td>{product.unit_price}</td>
+                                <td>{product.units_in_stock}</td>
+                                <td>{product.discontinued == 1 ? 'YES' : 'NO'}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>);
+    }
 }
--- front/components/Products.jsx
+++ front/components/Products.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import {useQuery} from "react-query";
+import {Link} from "react-router-dom";
 
 export function Products() {
-    return (<h1>Products</h1>);
+    const {data, isLoading} = useQuery("products", async ()=>{
+        const res = await fetch("http://127.0.0.1:9800/products", { method: "GET" });
+        return await res.json();
+    });
+
+    if(isLoading){
+        return (<div><h1>Loading...</h1></div>);
+    } else{
+        return (
+            <div>
+                <h1>Products</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th>Name</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map(product => (
+                            <tr key={product.product_id}>
+                                <td><Link to={`/product/${product.product_id}`}>{product.product_name}</Link></td>
+                            </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+            );
+    }
 }
