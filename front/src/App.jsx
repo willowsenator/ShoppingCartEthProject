be2a36c8f3a6f2ca73e176f791bfbc807e8b5ce8
willowsenator <willowsenator@gmail.com>
2023-10-14 21:13:04 +0200

    Modify Product component, create App component and create context for cart

---
+++ front/src/App.jsx
@@ -0,0 +1,32 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {Cart} from '../components/Cart.jsx';
+import {Products} from '../components/Products.jsx';
+import {Home} from '../components/Home.jsx';
+import {Product} from '../components/Product.jsx';
+import { createContext, useState } from 'react';
+
+export function App(){
+
+    const client = new QueryClient();
+    const Context = createContext(null);
+    const [status, setStatus] = useState({cart: []});
+
+    return (
+     <Context.Provider value={[status, setStatus]}>
+        <QueryClientProvider client={client}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}>
+                        <Route index element={<Products/>}/>
+                        <Route path='*' element={<Products/>}/>
+                        <Route path='products' element={<Products/>}/>
+                        <Route path='product/:id' element={<Product/>}/>
+                        <Route path='cart' element={<Cart/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </Context.Provider>
+    );
+}
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {Cart} from '../components/Cart.jsx';
-import {Products} from '../components/Products.jsx';
-import {Home} from '../components/Home.jsx';
-import {Product} from '../components/Product.jsx';
-
-const client = new QueryClient();
-
+import { App } from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}>
-            <Route index element={<Products/>}/>
-            <Route path='*' element={<Products/>}/>
-            <Route path='products' element={<Products/>}/>
-            <Route path='product/:id' element={<Product/>}/>
-            <Route path='cart' element={<Cart/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+  <App/>  
   </React.StrictMode>,
 );
