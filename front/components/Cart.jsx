2f690d5454a897b92228ad89f1199629e95c3aa8
willowsenator <willowsenator@gmail.com>
2023-10-16 15:26:57 +0200

    Add pay with ethWallet

--- front/components/Cart.jsx
+++ front/components/Cart.jsx
@@ -1,10 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../src/App.jsx";
+import {ethers} from "ethers";
+import { Link } from "react-router-dom";
 
 export function Cart() {
     const [status, setStatus] = useContext(Context);
+    const [account, setAccount] = useState(null);
+    const [txOk, setTxOk] = useState(null);
+    const [txKo, setTxKo] = useState(null);
 
     const cartTotalPrice = status.cart.reduce((accumulator, item)=> accumulator + item.total, 0);
+
+    useEffect(()=>{
+        window.ethereum && window.ethereum.request({
+            method: 'eth_requestAccounts'
+        }).then(accounts => {
+            setAccount(accounts[0]);
+            window.ethereum.on("accountsChanged",(accounts)=>{
+                setAccount(accounts[0]);
+            });
+        });
+    },[]);
+
+    function pay(){
+        const txParams = {
+            to: "",
+            from: account,
+            value: ethers.toBeHex(ethers.parseEther(cartTotalPrice.toString()))
+        };
+
+        try {
+            const tx = window.ethereum.request({
+                method: "eth_sendTransaction",
+                params: [txParams]
+            });
+
+            setTxOk(tx);
+        } catch (error) {
+            setTxKo(error);
+        }
+    }
 
     return (<div>
         <h1>Cart</h1>
@@ -21,7 +56,7 @@ export function Cart() {
             <tbody>
                 {status.cart.map(item => (
                     <tr key={item.product.product_id}>
-                        <td>{item.product.product_id}</td>
+                        <td><Link to={`/product/${item.product.product_id}`}>{item.product.product_id}</Link></td>
                         <td>{item.product.product_name}</td>
                         <td>{item.quantity}</td>
                         <td>{item.product.unit_price}</td>
@@ -30,6 +65,10 @@ export function Cart() {
                 ))}
             </tbody>
         </table>
-        <h2>Total {cartTotalPrice}</h2>
+        <h3>Total {cartTotalPrice}</h3>
+        <h4>Account: {account}</h4>
+        <button onClick={()=>pay()} className="btn btn-primary">Pay</button>
+        {txOk && <div className="alert alert-success">{txOk}</div>}
+        {txKo && <div className="alert alert-danger">{txKo}</div>}
     </div>);
 }
