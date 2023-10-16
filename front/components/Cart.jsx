544ba898d8f2aa0e9dd3a2fb40d42e41790976da
willowsenator <willowsenator@gmail.com>
2023-10-16 14:15:06 +0200

    Add cart page with the products

--- front/components/Cart.jsx
+++ front/components/Cart.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../src/App.jsx";
 
 export function Cart() {
-    return (<h1>Cart</h1>);
+    const [status, setStatus] = useContext(Context);
+
+    const cartTotalPrice = status.cart.reduce((accumulator, item)=> accumulator + item.total, 0);
+
+    return (<div>
+        <h1>Cart</h1>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>QUANTITY</th>
+                    <th>PRICE</th>
+                    <th>TOTAL</th>
+                </tr>
+            </thead>
+            <tbody>
+                {status.cart.map(item => (
+                    <tr key={item.product.product_id}>
+                        <td>{item.product.product_id}</td>
+                        <td>{item.product.product_name}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.product.unit_price}</td>
+                        <td>{item.total}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <h2>Total {cartTotalPrice}</h2>
+    </div>);
 }
